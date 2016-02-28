9f6408640ab54f9eabf2a6419f0140e6400dd0cd
fachriansyah <fachriansyah75@gmail.com>
2016-02-28 15:21:20 +0700

    Hello

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello{
+		public static void main (String[] sandikta){
+			int a = 10;
+		String nama = "fachriansyah";
+		String kelas = "TKJ 1";
+		System.out.println("nama saya = "+nama );
+		System.out.println("kelas saya = "+kelas);
+		System.out.println("cetak variable a "+a);
+		System.out.println("Hello sandikta ");
+		System.out.println("Hello sandikta ");
+		}
+}
